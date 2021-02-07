3434168b8c3cc745ff0a96f5e8c094070eb2e306
adwinugroho <adwinnugroho16@gmail.com>
2021-02-07 11:12:56 +0000

    initial service

--- services/services.go
+++ services/services.go
@@ -1,1 +1,28 @@
 package services
+
+import "github.com/adwinugroho/go-rest-self-screening/models"
+
+// Create a interface to implements our function at models
+type (
+	impelementsModels interface {
+		AddData(data models.HealthAssessment) (*models.HealthAssessment, error)
+		DeleteByKey(id string) (*[]models.HealthAssessment, error)
+		GetDataByKey(id string) (*models.HealthAssessment, error)
+		GetListAllData(vars models.BindVars) ([]models.HealthAssessment, int64, error)
+		SaveLog(model *models.HealthAssessment) (*string, error)
+		UpdateData(model *models.HealthAssessment) (*models.HealthAssessment, error)
+	}
+	// struct for reciever
+	HealthService struct {
+		dao impelementsModels
+	}
+)
+
+// Create a function to call implements models first
+func NewService(dao impelementsModels) *HealthService {
+	return &HealthService{dao}
+}
+
+func (service *HealthService) SubmitCovidScreening(reqData models.HealthAssessment) (*models.HealthAssessment, error) {
+	return &reqData, nil
+}
